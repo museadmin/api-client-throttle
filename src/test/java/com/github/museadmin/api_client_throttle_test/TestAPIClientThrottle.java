56a0cc861880811108feb7b3dc30c569a1109ff2
Bradley Atkins <bradley.atkins@bjss.com>
2019-07-13 13:56:38 +0100

    Added multithreaded unit test

--- src/test/java/com/github/museadmin/api_client_throttle_test/TestAPIClientThrottle.java
+++ src/test/java/com/github/museadmin/api_client_throttle_test/TestAPIClientThrottle.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import java.util.stream.IntStream;
 
 import static java.lang.Thread.sleep;
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.*;
 
 public class TestAPIClientThrottle {
 
@@ -26,7 +26,7 @@ public class TestAPIClientThrottle {
   public void testSetLatency() {
     APIClientThrottle throttle = new APIClientThrottle();
     throttle.setLatency(1000);
-    assertTrue(throttle.getLatency() == 1000);
+    assertEquals(1000, (int) throttle.getLatency());
   }
 
   @Test
@@ -34,14 +34,14 @@ public class TestAPIClientThrottle {
     APIClientThrottle throttle = new APIClientThrottle();
     throttle.setLatency(1000);
     throttle.resetLatency(200);
-    assertTrue(throttle.getLatency() == 200);
+    assertEquals(200, (int) throttle.getLatency());
   }
 
   @Test
   public void testRequestorsAreRegistered() {
     APIClientThrottle throttle = new APIClientThrottle();
     registerNumberOfRequestors(10, throttle);
-    assertTrue(throttle.numberOfRequestors() == 10);
+    assertEquals(10, (int) throttle.numberOfRequestors());
   }
 
   @Test
@@ -52,7 +52,7 @@ public class TestAPIClientThrottle {
     throttle.start();
     sleep(1000);
     throttle.stop();
-    assertTrue(throttle.numberOfQueueItems() == 0);
+    assertEquals(0, (int) throttle.numberOfQueueItems());
   }
 
   @Test
@@ -84,5 +84,76 @@ public class TestAPIClientThrottle {
 
     // Confirm we looped while waiting for Q to be processed
     assertTrue(loops > 150);
+  }
+
+  /**
+   * Test class mimics a service that is waiting for
+   * its turn to hit the head of the queue
+   */
+  class TestSvc implements Runnable {
+
+    private Thread t;
+    private String threadName;
+    public APIClientRequestor requestor;
+
+    public TestSvc(APIClientRequestor requestor, String name) {
+      this.requestor = requestor;
+      this.threadName = name;
+    }
+
+    public void run() {
+      int loops = 0;
+      while (requestor.inhibited) {
+        loops++;
+        try {
+          sleep(10);
+        } catch (InterruptedException e) {
+          System.out.println("Thread " +  threadName + " interrupted.");
+          break;
+        }
+      }
+    }
+
+    public void start () {
+      if (t == null) {
+        t = new Thread (this, threadName);
+        t.start ();
+      }
+    }
+  }
+
+  @Test
+  public void testThreadsWaitWhileInhibited() throws InterruptedException {
+
+    APIClientThrottle throttle = new APIClientThrottle();
+    throttle.setLatency(200);
+
+    // Add some bulk to the throttle load
+    registerNumberOfRequestors(5, throttle);
+
+    // One requestor for each thread
+    APIClientRequestor r1 = new APIClientRequestor();
+    APIClientRequestor r2 = new APIClientRequestor();
+    throttle.registerRequestor(r1);
+    throttle.registerRequestor(r2);
+    r1.enQueue();
+    r2.enQueue();
+
+    // Two runnable services to spawn the threads
+    TestSvc ts1 = new TestSvc(r1, "Thread1");
+    TestSvc ts2 = new TestSvc(r2, "Thread2");
+    ts1.start();
+    ts2.start();
+
+    // Start processing the request queue
+    throttle.start();
+    sleep(2000);
+    throttle.stop();
+
+    // Confirm all references were processed and dis-inhibited
+    assertFalse(ts1.requestor.inhibited);
+    assertFalse(ts2.requestor.inhibited);
+    assertFalse(r1.inhibited);
+    assertFalse(r2.inhibited);
   }
 }
